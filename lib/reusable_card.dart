b07e6a039b6dfc1e19adee0ce3fff57a8727e9e7
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2023-01-10 12:08:46 +0530

    Refactoring.

---
+++ lib/reusable_card.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour, this.cardChild});
+
+  final Color colour;
+  final Widget cardChild;
+  // Const should have data inside it before the run time and should only have data at compile time. When the app is being compiled to be used/distributed.
+  // However, final can have the value put inside it at runtime, it does not need a value at compile time. So a button, a widget, a box cannot be const but only be final when we are making our app.
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: cardChild,
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: colour,
+      ),
+    );
+  }
+}
